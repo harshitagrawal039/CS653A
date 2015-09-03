2f7de3e7ca9f0a5998fa9691df27a7577f92f0b5
Harshit Agrawal <harshitagrawal039@gmail.com>
2015-09-03 19:39:00 +0530

    Write fns aux(works), prettyPrint(does not work)

---
+++ PrettyPrint.hs
@@ -0,0 +1,45 @@
+--PrettyPrint.hs
+
+module PrettyPrint where
+
+type  Row = [String]
+type Table = [Row]
+type Width = [Int]
+
+-- Some helper functions
+pad :: Row -> Width ->[(Int, String)]
+pad t w1 = zipWith padSingle t w1
+	where
+		padSingle :: String -> Int -> (Int, String)
+		padSingle word size
+						| diff > 0 = (size, word ++ (replicate diff ' '))
+						| otherwise = (size - diff, word)
+						where
+							diff = size - length word
+
+splitRecordList :: [(a,b)] -> ([a], [b])
+splitRecordList [] = ([], [])
+splitRecordList ((l,r):xs) = (l:l1, r:r1)
+	where
+		(l1, r1) = splitRecordList xs
+
+-- An auxiliary function
+aux :: Table -> Width -> Width -> (Width, Table)
+aux [] _ w2 = (w2, [])
+aux (t:ts) w1 w2 = (w, t1) 
+	where
+		(ws, t2) = aux ts w1 w2
+		(wr, tr) = splitRecordList $ pad t w1
+		w = zipWith max ws wr
+		t1 = tr:t2
+
+-- prettyPrint function
+prettyPrint :: Table -> Table
+prettyPrint t = t1
+	where
+		(w, t1) = aux t w (replicate rowsize 0)
+		rowsize = length row
+		(row:_) = t
+
+table = [["hi", "hello", "hey"], ["ram", "she", "sundar"], ["aloo", "karela", "ab"]]
+a = aux table [7,7,7] [0,0,0]
