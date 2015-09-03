36790658b7211dfb90dd4ec51231e27afe681269
Harshit Agrawal <harshitagrawal039@gmail.com>
2015-09-03 11:41:59 +0530

    Add data type for including Infinity

--- MinTree.hs
+++ MinTree.hs
@@ -6,6 +6,16 @@ data Tree a = Empty
 			| Node (Tree a) a (Tree a)
 			deriving Show
 
+data Infinity a = Infinity
+				| Val a
+				deriving (Show, Eq)
+
+instance (Ord a) => Ord (Infinity a) where
+	compare Infinity Infinity	= EQ
+	compare Infinity _			= GT
+	compare _ Infinity			= LT
+	compare (Val a) (Val b)	= compare a b
+
 null :: Tree a -> Bool
 null Empty	= True
 null _		= False
@@ -16,10 +26,10 @@ empty = Empty
 singleton :: a -> Tree a
 singleton a = Node empty a empty
 
-infinity = read "Infinity" :: Int
+--infinity = read "Infinity" :: Int
 
 -- An auxiliary function
-aux :: (Ord a, Num a) => Tree a -> a -> a -> (a, Tree a)
+aux :: (Ord a) => Tree a -> a -> a -> (a, Tree a)
 aux Empty _ d = (d, empty)
 aux (Node lt x1 rt) x d = (m, Node lt1 x rt1)
 	where
@@ -28,8 +38,12 @@ aux (Node lt x1 rt) x d = (m, Node lt1 x rt1)
 		m = minimum [mlt, d, mrt, x1]
 
 -- minTree function
-minTree :: (Ord a, Num a) => Tree a -> Tree a
+minTree :: (Ord a) => Tree (Infinity a) -> Tree (Infinity a)
 minTree t = t1
 	where
-		(x,t1) = aux t x 1000
+		(x,t1) = aux t x Infinity
+
+{--
+a = Node (singleton $ Val 8) (Val 5) (Node (singleton $ Val 2) (Val 1) empty)
+--}
 
