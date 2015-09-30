c23572ed7edc3db2e823898e87b795634a6f87b2
Harshit Agrawal <harshitagrawal039@gmail.com>
2015-09-30 12:43:16 +0530

    Add helper fns: satisfy, (<|>), many

--- Parser.hs
+++ Parser.hs
@@ -1,6 +1,6 @@
 module Parser where
 
-import Control.Applicative
+import Control.Applicative hiding (many)
 
 data Result a	= ParseError
 				| Ok a String
@@ -12,10 +12,10 @@ instance Functor Result where
 	fmap f (Ok a str) = Ok (f a) str
 
 instance Functor Parser where
-	fmap f (Parser pa) = Parser (\x -> fmap f $ pa x)
+	fmap f (Parser pa) = Parser $ fmap f . pa
 
 instance Applicative Parser where
-	pure a = Parser (\x -> Ok a x)
+	pure a = Parser (Ok a)
 	(Parser pf) <*> (Parser pa) = Parser g
 		where
 			g x0 =	case pf x0 of
@@ -29,4 +29,29 @@ instance Monad Parser where
 			pb x0 = case pa x0 of
 						ParseError -> ParseError
 						Ok a rest  -> runParser (f a) rest
+
+-- Now define some helper functions
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy f = Parser g
+	where
+		g []	= ParseError
+		g (x:xs)	| f x			= Ok x xs
+					| otherwise 	= ParseError
+
+(<|>) :: Parser a -> Parser a -> Parser a
+(Parser pa1) <|> (Parser pa2) = Parser pa
+	where
+		pa x = case pa1 x of
+			ParseError	-> pa2 x
+			res 		-> res
+
+many :: Parser a -> Parser [a]
+many (Parser pa) = Parser g
+	where
+		g x0 = case pa x0 of
+			ParseError	-> Ok [] x0
+			Ok a x		-> Ok (a:resta) restx
+				where
+					(Ok resta restx) = (runParser $ many (Parser pa)) x
+
 
