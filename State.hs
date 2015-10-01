136467a92b6ed4f32b7b16d27a7e65ff8013ca10
Harshit Agrawal <harshitagrawal039@gmail.com>
2015-10-01 11:00:10 +0530

    Add State.hs, define State datatype and its functor, appllicative and monad instances

---
+++ State.hs
@@ -0,0 +1,23 @@
+module State where
+
+import Control.Applicative
+
+data State s a = State {runState :: s -> (a, s)}
+
+instance Functor (State s) where
+	fmap f (State h) = State g where
+		g s0 = (f a, s1) where
+			(a, s1) = h s0
+
+instance Applicative (State s) where
+	pure a = State (\x -> (a, x))
+	(State sf) <*> (State sa) = State g where
+		g s0 = (f a, s2) where
+			(f, s1) = sf s0
+			(a, s2) = sa s1
+
+instance Monad (State s) where
+	return = pure
+	(State sa) >>= f = State g where
+		g s0 = (runState . f) a s1  where
+			(a, s1) = sa s0
